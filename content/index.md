78813f5468b04d12354950845aa2866171811b08
JakeAiL <jakenguyen1501@gmail.com>
2024-10-20 23:49:23 +0700

    Quartz sync: Oct 20, 2024, 11:49 PM

---
+++ content/index.md
@@ -0,0 +1,16 @@
+---
+title: 
+type: 
+aliases: 
+tags:
+  - page/index
+  - page
+cssclasses:
+  - paper
+  - justified
+publish: true
+created: 20/10/2024 - 10:15 PM
+updated: 20/10/2024 - 11:32 PM
+encoded: Index.md
+link: 
+---
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://jakeail.github.io/AiL/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
