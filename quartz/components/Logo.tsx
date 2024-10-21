cf6ee4ff06855a421b4df1af2f9c2b07d33cc3db
Jake <jakenguyen1501@gmail.com>
2024-10-21 22:40:58 +0700

    Update Logo.tsx

--- quartz/components/Logo.tsx
+++ quartz/components/Logo.tsx
@@ -12,7 +12,7 @@ function Logo({ cfg, fileData, externalResources, displayClass }: QuartzComponen
 
   return (
     <div class={classNames(displayClass, "logo logo-container")}>
-      <img rel="icon" src={content/z-attachment/logo.png} class="logo logo-image" />
+      <img rel="icon" src={content/z-attachment} class="logo logo-image" />
     </div>
   )
 }
