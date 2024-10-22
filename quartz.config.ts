6ec628ba2019f5ffe302fc140e540db0da07fe06
Jake <jakenguyen1501@gmail.com>
2024-10-23 03:29:41 +0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Artist is Lazy",
+    pageTitle: "Artist   is   Lazy",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
